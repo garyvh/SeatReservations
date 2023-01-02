04a0bcd394321fae15b041792eade7e7864c2bbc
Gary Hoang <garyhoangvinh@gmail.com>
2023-01-02 09:16:12 -0800

    SeatReservationsScale.java v1 and SeatReservationsTest v1, added another implementation of SeatReservations that is more scaleable to more different plane seat layouts

---
+++ SeatReservationsScale.java
@@ -0,0 +1,189 @@
+import java.util.*;
+
+public class SeatReservationsScale {
+
+    // Map to convert Letter to its corresponding index
+    public static Map<Character, Integer> LETTER_TO_INDEX = new HashMap<>() {{
+        put('A', 0);
+        put('B', 1);
+        put('C', 2);
+        put('D', 3);
+        put('E', 4);
+        put('F', 5);
+        put('G', 6);
+        put('H', 7);
+        put('J', 8);
+        put('K', 9);
+        put('L', 10);
+        put('M', 11);
+        put('N', 12);
+        put('O', 13);
+        put('P', 14);
+        put('Q', 15);
+        put('R', 16);
+        put('S', 17);
+        put('T', 18);
+        put('U', 19);
+        put('V', 20);
+        put('W', 21);
+        put('X', 22);
+        put('Y', 23);
+        put('Z', 24);
+    }};
+
+    /*
+        returns the maximum amount of families that can be seated together in
+        a given airplane seating layout
+
+        Parameters:
+            boolean[][] seats: The grid (2D array) that represents the seating
+                               arrangement of the plane. seats[i][j] must indicate
+                               if the seat is reserved or not (true if reserved,
+                               false otherwise). Supports irregular layouts where
+                               there can be missing seats in a grid like the following:
+                                       A B C
+                                    1 [][][]
+                                    2 []  []
+                                    3 [][][]
+                                A hole in the grid is indicated with a true value, for
+                                example, if my plane is missing the 2B seat I would still
+                                have a 2D matrix of row size 3 and col size 3, but I would
+                                make sure that seats[1][1] = true before passing seats into
+                                this function.
+
+            boolean[][] aisles: boolean array that indicates where the aisles are. A true
+                                value at index i means there is an aisle between i and
+                                i + 1. Must have size equal to the maximum number of columns
+                                seats has, minus 1.
+
+            String input: string containing all seats that is already occupied before
+                          computation of maximum number of families.
+
+        Returns: Type int, maximum amount of families that can be seated together
+        Throws: IllegalArgumentException if input is null or seats is null or aisles is
+                null or seats has length of 0 or aisles.length != max columns size of
+                grid minus 1.
+
+        s = length of input string, r = # of rows, c = # of columns
+        Runtime: O(s + rc)
+        Space: O(1), note that seats will likely be mutated
+     */
+    public static int getMaxFamilies(boolean[][] seats, boolean[] aisles, String input) {
+        if (input == null) {
+            throw new IllegalArgumentException("input String to getMaxFamilies is null");
+        }
+
+        if (seats == null) {
+            throw new IllegalArgumentException("parameter boolean[][] seats is null");
+        }
+
+        if (aisles == null) {
+            throw new IllegalArgumentException("parameter boolean[] aisles is null");
+        }
+
+        if (seats.length == 0) {
+            throw new IllegalArgumentException("parameter boolean[][] seats is empty");
+        }
+
+        if (aisles.length != seats[0].length - 1) {
+            throw new IllegalArgumentException("parameter boolean[] aisles is incorrect length");
+        }
+
+        seatReserved(seats, input);
+        int max = 0;
+        int rowIndex = 0;
+
+        /*
+            Runtime (outer loop): O(r) where r = number of rows
+            Runtime (inner loop): O(c) where c = number of cols
+            Runtime (Total): O(rc)
+         */
+        while (rowIndex < seats.length) {
+            boolean[] row = seats[rowIndex];
+            int colIndex = 0;
+
+            while (colIndex < seats[rowIndex].length - 3) {
+                boolean currSeatTaken = row[colIndex];
+                boolean sndSeatTaken = row[colIndex + 1];
+                boolean thdSeatTaken = row[colIndex + 2];
+                boolean fthSeatTaken = row[colIndex + 3];
+
+                // If current seat and next 3 seats are not taken
+                if (!currSeatTaken && !sndSeatTaken && !thdSeatTaken && !fthSeatTaken) {
+
+                    /*
+                        Example: 1A1B1C1D
+                        if there is not an aisle between 1A and 1B, and
+                        no aisle between 1C and 1D.
+                        (aisle between 1B and 1C is allowed)
+                     */
+                    if (!aisles[colIndex] && !aisles[colIndex + 2]) {
+                        max++;
+                        colIndex += 4;
+                    } else {
+                        colIndex++;
+                    }
+
+                } else {
+                    colIndex += findFurthestNonTakenSeat(currSeatTaken, sndSeatTaken,
+                                                        thdSeatTaken, fthSeatTaken);
+                }
+            }
+            rowIndex++;
+        }
+
+        return max;
+    }
+
+    /*
+        Given a string that tells what seats are reserved and 2D boolean
+        array, marks the reserved seats on the 2D boolean array by setting
+        them to true.
+
+        Mutates seats.
+
+        Parameters:
+            boolean[][] seats: the airplane seats layout
+            String input: The string indicating which seats are reserved
+                          beforehand
+
+        s = length of input string
+        Runtime: O(s)
+        Space: O(1)
+     */
+    public static void seatReserved(boolean[][] seats, String input) {
+        for (int i = 0; i < input.length(); i += 3) {
+            int rowNumber = Character.getNumericValue(input.charAt(i));
+            int col = LETTER_TO_INDEX.get(input.charAt(i + 1));
+            seats[rowNumber - 1][col] = true;
+        }
+    }
+
+    /*
+        Given 4 seats, returns an int that indicates how much seats we
+        are able to safely skip (we can safely skip all seats that
+        are taken but not ones that are not taken)
+
+        Parameters:
+            boolean fst: first seat
+            boolean snd: second seat
+            boolean thd: third seat
+            boolean fth: fourth seat
+
+        Returns: type int, how many seats we can skip.
+
+        Runtime: O(1)
+        Space: O(1)
+     */
+    private static int findFurthestNonTakenSeat(boolean fst, boolean snd, boolean thd, boolean fth) {
+        if (fth) {
+            return 4;
+        } else if (thd) {
+            return 3;
+        } else if (snd) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+}
---
+++ SeatReservationsScaleTest.java
@@ -0,0 +1,275 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SeatReservationsScaleTest {
+    @Test
+    void testEmptyString() {
+        boolean[][] seats = new boolean[5][10];
+        boolean[] aisles = new boolean[9];
+        aisles[2] = true;
+        aisles[6] = true;
+        String test = "";
+
+        assertEquals(10, SeatReservationsScale.getMaxFamilies(seats, aisles, test));
+    }
+
+    @Test
+    void testNoSeats() {
+        boolean[][] seats = new boolean[5][10];
+        boolean[] aisles = new boolean[9];
+        aisles[2] = true;
+        aisles[6] = true;
+        String test = "1A 1B 1C 1D 1E 1F 1G 1H 1J 1K 2A 2B 2C 2D 2E 2F 2G 2H 2J 2K 3A 3B 3C 3D 3E 3F 3G 3H 3J 3K 4A 4B 4C 4D 4E 4F 4G 4H 4J 4K 5A 5B 5C 5D 5E 5F 5G 5H 5J 5K";
+
+        assertEquals(0, SeatReservationsScale.getMaxFamilies(seats, aisles, test));
+    }
+
+    @Test
+    void test_1A_2F_1C_3E_4F_5H() {
+        boolean[][] seats = new boolean[5][10];
+        boolean[] aisles = new boolean[9];
+        aisles[2] = true;
+        aisles[6] = true;
+        String test = "1A 2F 1C 3E 4F 5H";
+
+        assertEquals(5, SeatReservationsScale.getMaxFamilies(seats, aisles, test));
+    }
+
+    @Test
+    void test_5E_5F_4E_4F_3E_3F_2E_2F_1E_1F() {
+        boolean[][] seats = new boolean[5][10];
+        boolean[] aisles = new boolean[9];
+        aisles[2] = true;
+        aisles[6] = true;
+        String test = "5E 5F 4E 4F 3E 3F 2E 2F 1E 1F";
+
+        assertEquals(0, SeatReservationsScale.getMaxFamilies(seats, aisles, test));
+    }
+
+    @Test
+    void test_4J_3J_2J_1J_5J() {
+        boolean[][] seats = new boolean[5][10];
+        boolean[] aisles = new boolean[9];
+        aisles[2] = true;
+        aisles[6] = true;
+        String test = "4J 3J 2J 1J 5J";
+
+        assertEquals(5, SeatReservationsScale.getMaxFamilies(seats, aisles, test));
+    }
+
+    @Test
+    void test_1B_1E_3A_3D_2H_4C_4E_5E() {
+        boolean[][] seats = new boolean[5][10];
+        boolean[] aisles = new boolean[9];
+        aisles[2] = true;
+        aisles[6] = true;
+        String test = "1B 1E 3A 3D 2H 4C 4E 5E";
+
+        assertEquals(5, SeatReservationsScale.getMaxFamilies(seats, aisles, test));
+    }
+
+    @Test
+    void testDEFGOnly() {
+        boolean[][] seats = new boolean[5][10];
+        boolean[] aisles = new boolean[9];
+        aisles[2] = true;
+        aisles[6] = true;
+        String test = "1A 1F 2A 2B 2G 3F 4F 4J 5A 5G 5K";
+
+        assertEquals(4, SeatReservationsScale.getMaxFamilies(seats, aisles, test));
+    }
+
+    @Test
+    void testFGHJOnly() {
+        boolean[][] seats = new boolean[5][10];
+        boolean[] aisles = new boolean[9];
+        aisles[2] = true;
+        aisles[6] = true;
+        String test = "1A 1B 1C 1D 1E 1K 2B 2E 2K 3C 3D 4D 5C 5D 5G";
+
+        assertEquals(4, SeatReservationsScale.getMaxFamilies(seats, aisles, test));
+    }
+
+    @Test
+    void testMixed() {
+        boolean[][] seats = new boolean[5][10];
+        boolean[] aisles = new boolean[9];
+        aisles[2] = true;
+        aisles[6] = true;
+        String test = "1A 1K 3D 4C 4H 5F";
+
+        assertEquals(7, SeatReservationsScale.getMaxFamilies(seats, aisles, test));
+    }
+
+    @Test
+    void testStringRandomization() {
+        boolean[][] seats = new boolean[5][10];
+        boolean[] aisles = new boolean[9];
+        aisles[2] = true;
+        aisles[6] = true;
+        String test1 = "1D 1G 2C";
+        String test2 = "1G 1D 2C";
+        String test3 = "2C 1G 1D";
+        String test4 = "1G 2C 1D";
+
+        assertEquals(7, SeatReservationsScale.getMaxFamilies(seats, aisles, test1));
+        assertEquals(7, SeatReservationsScale.getMaxFamilies(seats, aisles, test2));
+        assertEquals(7, SeatReservationsScale.getMaxFamilies(seats, aisles, test3));
+        assertEquals(7, SeatReservationsScale.getMaxFamilies(seats, aisles, test4));
+    }
+
+    @Test
+    void testNullString() {
+        boolean[][] seats = new boolean[5][10];
+        boolean[] aisles = new boolean[9];
+        aisles[2] = true;
+        aisles[6] = true;
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            assertEquals(7, SeatReservationsScale.getMaxFamilies(seats, aisles, null));
+        });
+    }
+
+    @Test
+    void testEmptyArrays() {
+        boolean[][] seats = new boolean[0][0];
+        boolean[] aisles = new boolean[0];
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            assertEquals(7, SeatReservationsScale.getMaxFamilies(seats, aisles, ""));
+        });
+    }
+
+    @Test
+    void testLargerAisles() {
+        boolean[][] seats = new boolean[1][1];
+        boolean[] aisles = new boolean[2];
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            assertEquals(7, SeatReservationsScale.getMaxFamilies(seats, aisles, ""));
+        });
+    }
+
+    @Test
+    void testSmallNoReservationsNoAisles() {
+        boolean[][] seats = new boolean[4][4];
+        boolean[] aisles = new boolean[3];
+        String test = "";
+
+        assertEquals(4, SeatReservationsScale.getMaxFamilies(seats, aisles, test));
+    }
+
+    @Test
+    void testSmallNoFreeSeatsNoAisle() {
+        boolean[][] seats = new boolean[4][4];
+        boolean[] aisles = new boolean[3];
+        String test = "1A 1B 1C 1D 2A 2B 2C 2D 3A 3B 3C 3D 4A 4B 4C 4D";
+
+        assertEquals(0, SeatReservationsScale.getMaxFamilies(seats, aisles, test));
+    }
+
+    @Test
+    void testSmallNoReservationsMiddleAisle() {
+        boolean[][] seats = new boolean[4][4];
+        boolean[] aisles = new boolean[3];
+        aisles[1] = true;
+        String test = "";
+
+        assertEquals(4, SeatReservationsScale.getMaxFamilies(seats, aisles, test));
+    }
+
+    @Test
+    void testSmallNoReservationsLeftAisle() {
+        boolean[][] seats = new boolean[4][4];
+        boolean[] aisles = new boolean[3];
+        aisles[0] = true;
+        String test = "";
+
+        assertEquals(0, SeatReservationsScale.getMaxFamilies(seats, aisles, test));
+    }
+
+    @Test
+    void testSmallNoReservationsRightAisle() {
+        boolean[][] seats = new boolean[4][4];
+        boolean[] aisles = new boolean[3];
+        aisles[2] = true;
+        String test = "";
+
+        assertEquals(0, SeatReservationsScale.getMaxFamilies(seats, aisles, test));
+    }
+
+    @Test
+    void testSmallNoReservationsMaxAisles() {
+        boolean[][] seats = new boolean[4][4];
+        boolean[] aisles = new boolean[3];
+        aisles[0] = true;
+        aisles[1] = true;
+        aisles[2] = true;
+        String test = "";
+
+        assertEquals(0, SeatReservationsScale.getMaxFamilies(seats, aisles, test));
+    }
+
+    @Test
+    void testSmallIrregularLayout() {
+        boolean[][] seats = new boolean[4][4];
+        seats[1][3] = true;
+        seats[2][1] = true;
+        seats[2][2] = true;
+        seats[2][3] = true;
+        seats[3][0] = true;
+        seats[3][1] = true;
+        seats[3][2] = true;
+        seats[3][3] = true;
+        boolean[] aisles = new boolean[3];
+        String test = "";
+
+        assertEquals(1, SeatReservationsScale.getMaxFamilies(seats, aisles, test));
+    }
+
+    @Test
+    void testSmallIrregularLayoutTakenSeat() {
+        boolean[][] seats = new boolean[4][4];
+        seats[1][3] = true;
+        seats[2][1] = true;
+        seats[2][2] = true;
+        seats[2][3] = true;
+        seats[3][0] = true;
+        seats[3][1] = true;
+        seats[3][2] = true;
+        seats[3][3] = true;
+        boolean[] aisles = new boolean[3];
+        String test = "1A";
+
+        assertEquals(0, SeatReservationsScale.getMaxFamilies(seats, aisles, test));
+    }
+
+    @Test
+    void testManyColsManyAisles() {
+        boolean[][] seats = new boolean[1][20];
+        boolean[] aisles = new boolean[19];
+        aisles[2] = true;
+        aisles[5] = true;
+        aisles[8] = true;
+        aisles[11] = true;
+        aisles[14] = true;
+        aisles[17] = true;
+        String test = "";
+
+        assertEquals(3, SeatReservationsScale.getMaxFamilies(seats, aisles, test));
+    }
+
+    @Test
+    void testManyColsSomeAislesTakenSeat() {
+        boolean[][] seats = new boolean[1][20];
+        boolean[] aisles = new boolean[19];
+        aisles[0] = true;
+        aisles[2] = true;
+        aisles[17] = true;
+        aisles[18] = true;
+        String test = "1M";
+
+        assertEquals(3, SeatReservationsScale.getMaxFamilies(seats, aisles, test));
+    }
+}
